0a62537a679bb14c56555525d0e5c42a47b17819
morganegmd <morgane.grimaud112@gmail.com>
2021-12-27 00:44:50 +0100

    product api

--- front/js/product.js
+++ front/js/product.js
@@ -1,37 +1,9 @@
 /*localhost:3000/api/products/
 https://www.youtube.com/watch?v=li7Fmtk4RCo*/
 
-/*const fetchProduct = async () => {
-  let fetchDataProduct = await fetch("http://localhost:3000/api/products");
-  let productData = await fetchDataProduct.json();
-  return productData;
-};
-console.log(fetchProduct());
-
-const productView = async () => {
-  let productCanap = await fetchProduct();
-  console.log(productCanap());
-  let itemsImg = document.getElementById("items_img");
-  let description = document.getElementById("description");
-  let colors = document.getElementById("colors");
-  for (let i = 0; i < canapeProduct.length; i += 1) {
-    console.log(productCanap[i]);
-    itemsImg.innerHTML = `<img src="product.html?id=${productCanap[i]._id}" alt="${productCanap[i].altTxt}"></img>`;
-    description.innerHTML = `"${productCanap[i].description}"`;
-    colors.innerHTML = `"${productCanap[i].colors}"`;
-  }
-};*/
-
 const product = window.location.search;
 
 console.log(product);
-
-/*let urlData = new URL(
-  "http://localhost:3000/api/products/a557292fe5814ea2b15c6ef4bd73ed83"
-);
-console.log(urlData);*/
-
-let productData = [];
 
 const fetchProduct = async () => {
   let fetchDataProduct = await fetch(
@@ -40,5 +12,24 @@ const fetchProduct = async () => {
   let productData = await fetchDataProduct.json();
   return productData;
 };
+
 fetchProduct();
 console.log(fetchProduct());
+
+const canapeView = async () => {
+  let canapeProductView = await fetchProduct();
+  console.log(canapeProductView());
+  let itemImg = document.getElementById("item__img");
+  let title = document.getElementById("title");
+  let price = document.getElementById("price");
+  let description = document.getElementById("description");
+  let colors = document.getElementById("colors");
+  for (let i = 0; i < canapeProductView.length; i += 1) {
+    console.log(canapeProduct[i]);
+    itemImg.innerHTML += `<img src="${canapeProductView[i].imageUrl}" alt="${canapeProductView[i].altTxt}" />`;
+    title.innerHTML += `<h1 id="title">"${canapeProductView[i].name}"</h1>`;
+    price.innerHTML += `"${canapeProductView[i].price}"`;
+    description.innerHTML += `"${canapeProductView[i].description}" `;
+    colors.innerHTML += `"${canapeProductView[i].colors}"`;
+  }
+};
