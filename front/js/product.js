bb16e8bc758736734375bc697f7cbe5a0485d6b9
morganegmd <morgane.grimaud112@gmail.com>
2021-12-28 23:29:46 +0100

    pb colors

--- front/js/product.js
+++ front/js/product.js
@@ -27,28 +27,25 @@ const canapeViews = async () => {
   let title = document.getElementById("title");
   let price = document.getElementById("price");
   let description = document.getElementById("description");
-  let colors = document.getElementById("colors");
+
   itemImg.innerHTML += `<img src="${canapeProductView.imageUrl}" alt=${canapeProductView.altTxt} />`;
   title.innerHTML += `<h1 id="title">${canapeProductView.name}</h1>`;
   price.innerHTML += `${canapeProductView.price}`;
   description.innerHTML += `${canapeProductView.description}`;
-  colors.innerHTML += `${canapeProductView.colors}`;
 };
 
 canapeViews();
 
 /*Insérer les couleurs*/
 
-colors.forEach((colorViewer) => {
-  colors.appendChild;
-});
-
-/*const colors = async () => {
-  let canapeProductView = await fetchProduct();
-  console.log(canapeProductView);
-  colorsall = document.getElementById("colors");
-  for (let i = 0; i < colors.length; i += 1) {
-    colors.innerHTML += `${canapeProductView.colors}`;
+const color = async () => {
+  let colorsCanapeView = await fetchProduct();
+  console.log(colorsCanapeView);
+  let option = document
+    .getElementById("colors")
+    .getElementsByTagName("value")[0].innerHTML;
+  for (let colorsall of fetchProduct) {
+    option.innerHTML += `${colorsProductView.colors}`;
   }
 };
-colors();*/
+color();
