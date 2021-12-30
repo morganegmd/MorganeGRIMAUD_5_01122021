3c38a48f60f1b61f19568b8481ba9429f15bd270
morganegmd <morgane.grimaud112@gmail.com>
2021-12-30 18:24:51 +0100

    localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -32,20 +32,71 @@ const canapeViews = async () => {
   title.innerHTML += `<h1 id="title">${canapeProductView.name}</h1>`;
   price.innerHTML += `${canapeProductView.price}`;
   description.innerHTML += `${canapeProductView.description}`;
+  color(canapeProductView.colors);
+  addCart(canapeProductView);
 };
 
 canapeViews();
 
 /*Insérer les couleurs*/
 
-const color = async () => {
-  let colorsCanapeView = await fetchProduct();
-  console.log(colorsCanapeView);
-  let option = document
-    .getElementById("colors")
-    .getElementsByTagName("value")[0].innerHTML;
-  for (let colorsall of fetchProduct) {
-    option.innerHTML += `${colorsProductView.colors}`;
+const color = (colors) => {
+  let option = document.getElementById("colors");
+  for (let color of colors) {
+    option.innerHTML += `<option value="${color}">${color}</option>`;
   }
 };
-color();
+
+/*Enregistrer les données saisies par l'utilisateur 
+  L'alerter en cas d'erreur de saisie
+  Sauvegarder dans le local storage
+  Envoyer ses informations dans le panier*/
+
+const addCart = (canapeProductView) => {
+  let bouton = document.getElementById("addToCart");
+  bouton.addEventListener("click", () => {
+    console.log("bouton");
+    let color = document.getElementById("colors").value;
+    let quantity = document.getElementById("quantity").value;
+    console.log(color, quantity);
+    let information = `${color}``${quantity}`;
+    if (!color) {
+      alert("Veuillez choisir une couleur");
+      return;
+    }
+    if (!(quantity > 0 && quantity < 101)) {
+      alert("Veuillez choisir une quantité entre 1 et 100");
+      return;
+    }
+    if ((color == null, quantity == null)) {
+      localStorage.setItem("color", `${color}`);
+      localStorage.setItem("number", `${quantity}`);
+    } else {
+      information.push();
+      localStorage.getItem("color");
+      localStorage.getItem("number");
+    }
+
+    /*régler le pb de l'ajout*/
+  });
+};
+
+/*if ((color == null, quantity == null)) {
+      localStorage.setItem("color", `${color}`);
+      localStorage.setItem("nombre", `${quantity}`);
+    } else {
+      localStorage.setItem("color", `${color}`);
+      localStorage.setItem("nombre", `${quantity}`);
+    }
+    
+    
+     let information = `${color}``${quantity}`;
+    if ((color == null, quantity == null)) {
+      localStorage.setItem("color", `${color}`);
+      localStorage.setItem("nombre", `${quantity}`);
+    } else {
+      color.push();
+      quantity.push();
+      localStorage.setItem("color", `${color}`);
+      localStorage.setItem("nombre", `${quantity}`);
+    }*/
